31e2eadc07a39e053dd428b44f7a8085b1f56d62
Brendan Sullivan <brendansully3@gmail.com>
2023-09-26 14:37:01 -0600

    GE03 part 1

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Student
+
+# Register your models here so they can be edited in admin panel
+admin.site.register(Student)
